c0d987002947d4eb1b9659386a397d4b91060308
jqbFrnzs <jacobfz@tutanota.com>
2023-01-07 13:23:58 +0100

    added user authentication based on defined .conf files both for server and client side

--- DFS4/dfs4.py
+++ DFS4/dfs4.py
@@ -8,6 +8,7 @@ January 2023
 """
 
 import sys
+import re
 
 # function to check port number assignment
 def check_args():
@@ -29,3 +30,34 @@ def check_args():
 		except ValueError:
 				print("ERROR: Port number must be a number.")
 				sys.exit()
+
+# get authentication parameters
+def auth_params():
+
+	# use dfs configuration file 
+	config_file='dfs.conf'
+
+	# get usernames from config file 
+	fh=open(config_file, mode='r', encoding='cp1252')
+	users=re.findall(r'Username: .*', fh.read())
+	usernames=list()
+	for i in range(0, len(users)):
+		usernames.append(str(users[i]).split()[1])
+	fh.close()
+
+	# get passwords from config file
+	fh=open(config_file, mode='r', encoding='cp1252')
+	passes=re.findall(r'Password: .*', fh.read())
+	passwords=list()
+	for i in range(0, len(passes)):
+		passwords.append(str(passes[i]).split()[1])
+	fh.close()
+
+	# create dict with usernames:passwords 
+	global auth_dict 
+	auth_dict = {}
+	for i in range(0, len(users)):
+		entry={usernames[i]:passwords[i]}
+		auth_dict.update(entry)
+
+	return auth_dict
