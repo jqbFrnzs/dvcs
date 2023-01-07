cde4c8f1650d8fc8ad045062ac87a6142962d56e
jqbFrnzs <jacobfz@tutanota.com>
2023-01-07 13:26:15 +0100

    added client-side auth

--- DFC/dfc.py
+++ DFC/dfc.py
@@ -8,6 +8,7 @@ January 2023
 """
 import sys
 import os
+import re
 
 # check argument to open dfc.conf 
 def check_args():
@@ -30,6 +31,88 @@ def check_args():
 	# if no error, return dfc.conf 
 	else:
 		return sys.argv[1]
+
+# params for user auth from dfc.conf
+def user_auth():
+	
+	# get usernames
+	fh = open('dfc.conf', mode='r', encoding='cp1252')
+	users=re.findall(r'Username: .*', fh.read())
+	usernames=list()
+	for i in range(0, len(users)):
+		usernames.append(str(users[i]).split()[1])
+	fh.close()
+		
+	# get passwords 
+	fh = open('dfc.conf', mode='r', encoding='cp1252')
+	passes=re.findall(r'Password: .*', fh.read())
+	passwords=list()
+	for i in range(0, len(passes)):
+		passwords.append(str(passes[i]).split()[1])
+	fh.close()
+
+	# dict with usernames:passwords 
+	global auth_dict 
+	auth_dict = {}
+	for i in range(0, len(users)):
+		entry={usernames[i]:passwords[i]}
+		auth_dict.update(entry)
+	
+	return auth_dict
+
+# client-side auth
+def authenticate():
+	
+	# config params
+	user_auth()
+	
+	# authenticate username
+	
+	# initialize an auth status 
+	auth_status = ''
+	
+	# give user 4 attempts
+	for i in range(0, 4):
+		if auth_status == 'Valid username.':
+			# go to password auth 
+			pass
+		
+		else:
+			# get username 
+			username = input('username: ')
+		
+			# initialize username auth 
+			username_auth = []
+			ct = 0
+			for key, value in auth_dict.items():
+				ct += 1
+				if username == key:
+					# get specific username index in dictionary 
+					username_auth.append(ct)
+				else:
+					username_auth.append(0)
+							
+			if i < 2:
+				if sum(username_auth) > 0:
+					auth_status = 'Valid username.'
+					continue
+				else:
+					print('Username does not exist. You have ' +str(3-i) + ' attempts left.')
+					continue 
+			elif i == 2:
+				if sum(username_auth) > 0:
+					auth_status = 'Valid username.'
+					continue
+				else:
+					print('Username does not exist. You have ' +str(3-i) + ' attempt left.')
+					continue 				
+			else:
+				if sum(username_auth) > 0:
+					auth_status = 'Valid username.'
+					continue
+				else:
+					print('Username does not exist. You have no more attempts.\nExiting now....')
+					sys.exit()
 
 # run client
 if __name__=='__main__':
